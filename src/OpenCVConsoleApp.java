a04183f0f1dc9715063158e7f1973ce895ddc822
christopherbailey <c.bailey1@uqconnect.edu.au>
2022-08-08 21:06:50 +1000

    Initial face and landmark detection implemented, models still to be trained.

--- src/OpenCVConsoleApp.java
+++ src/OpenCVConsoleApp.java
@@ -1,11 +1,18 @@
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
+import org.opencv.core.*;
+import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.face.Facemark;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.videoio.VideoCapture;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.opencv.face.Face.createFacemarkKazemi;
+import static org.opencv.imgproc.Imgproc.*;
+
 /**
- * The OpenCVConsoleApp class implements an application that reads from a
- * camera and performs drowsiness detection.
+ * The OpenCVConsoleApp class implements an application that reads from a camera
+ * and performs drowsiness detection.
  *
  * <p>This is accomplished by applying facial landmark detection to extract the
  * eye regions and computing the eye aspect ratio. A lower ratio indicates
@@ -21,6 +28,8 @@ public class OpenCVConsoleApp {
 
         Camera camera = new Camera();
         camera.captureFrame();
+        camera.detectLandmarks();
+        camera.writeImage();
     }
 }
 
@@ -48,10 +57,76 @@ class Camera {
     }
 
     /**
+     * Detect face landmarks in the currently captured image
+     *
+     * <p>A cascade classifier is used to perform face detection while a OpenCV
+     * Facemark model is used for facial landmark recognition.
+     */
+    public void detectLandmarks() {
+        // TODO: train models
+        String cascade_filename = "abc";
+        String facemark_filename = "abc";
+
+        // Load model
+        CascadeClassifier face_cascade = new CascadeClassifier();
+        face_cascade.load(cascade_filename);
+        Facemark facemark = createFacemarkKazemi();
+        facemark.loadModel(facemark_filename);
+
+        // Perform face detection
+        MatOfRect faces = new MatOfRect();
+        resize(
+                image,
+                image,
+                new Size(480, 480),
+                0,
+                0,
+                INTER_LINEAR_EXACT
+        );
+
+        Mat gray = new Mat();
+        if(image.channels() > 1) {
+            cvtColor(image, gray, COLOR_BGR2GRAY);
+        } else {
+            gray = image.clone();
+        }
+        equalizeHist(gray, gray);
+
+        face_cascade.detectMultiScale(
+                gray,
+                faces,
+                1.1,
+                3,
+                0,
+                new Size(30, 30)
+        );
+
+        // Extract facial landmarks and draw face boundaries and landmarks
+        List<MatOfPoint2f> shapes = new ArrayList<>();
+
+        if (facemark.fit(image, faces, shapes)) {
+            for (int i = 0; i < faces.toArray().length; i++) {
+                // draw detected face rectangle
+                rectangle(image, faces.toArray()[i], new Scalar(255,0,0));
+
+                for (int j = 0; j < shapes.size(); j++) {
+                    circle(
+                            image,
+                            shapes.get(i).toArray()[j],
+                            5,
+                            new Scalar(0,0,255),
+                            FILLED
+                    );
+                }
+            }
+        }
+    }
+
+    /**
      * Write the current captured image from the camera
      */
     public void writeImage() {
-        String filename = "shot.jpg";
+        String filename = "output.jpg";
         Imgcodecs.imwrite(filename, image);
     }
 }
